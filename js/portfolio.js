fb94a52b957203c284a6ec4a135b07422ac73f36
joneskj55 <joneskj55@gmail.com>
2020-08-09 20:53:32 -0500

    side-nav disappears down the page

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,4 +14,23 @@ $(document).ready(function () {
 //-------DATE & TIME----------//
     var currentTime = new Date();
     var year = currentTime.getFullYear();
+
+    function share() {
+        var winWidth = $(window).width();
+        if (winWidth > 999 ) {
+            var docHeight = $(document).height();
+            var top = $(document).scrollTop();
+            if (top > docHeight - 1740 ) {
+                $('.share-box').fadeOut(200);
+            } else {
+                $('.share-box').fadeIn(200);
+            }
+        } else {
+            $('.share-box').hide();
+        }
+    }
+
+    $(window).scroll(function () {
+        share();
+    });
 });

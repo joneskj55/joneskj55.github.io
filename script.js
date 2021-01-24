faa8f787e5ae2f611af65289d393072c742a62de
joneskj55 <joneskj55@gmail.com>
2021-01-23 18:28:50 -0600

    update subheader text

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const typewriter = new Typewriter(subHead, {
 
 typewriter
     .pauseFor(1500)
-    .typeString('<span style="color: ivory">Open</span> Source <span style="color: ivory">Contributor</span>')
+    .typeString('<span style="color: #00cfff">Ohio</span> <span style="color: ivory">Kid</span>')
     .pauseFor(300)
-    .deleteChars(25)
+    .deleteChars(8)
+    .typeString('<span style="color: ivory">Soldier</span>')
+    .pauseFor(300)
+    .deleteChars(7)
+    .typeString('<span style="color: ivory">Veteran</span>')
+    .pauseFor(300)
+    .deleteChars(7)
     .typeString('<span style="color: #00cfff;">Software</span> <span style="color: ivory">Engineer</span')
     .pauseFor(1000)
     .start();

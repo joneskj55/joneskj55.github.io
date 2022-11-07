462ce0184060d85b374f3adc6103e583b977a969
joneskj55 <joneskj55@gmail.com>
2022-11-07 14:56:07 -0600

    add serviceWorker and initial menu page

--- joneskj55.github.io/src/App.js
+++ joneskj55.github.io/src/App.js
@@ -1,24 +1,60 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateContent from "./components/CreateContent";
+import EditContent from "./components/EditContent";
+import ContentList from "./components/ContentList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Tailwind added!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+            <Link to={"/create-content"} className="nav-link">
+              React MERN Stack App
+            </Link>
+
+            <nav className="justify-content-end">
+              <nav>
+                <Link to={"/create-content"} className="nav-link">
+                  Create Content
+                </Link>
+              </nav>
+              <nav>
+                <Link to={"/content-list"} className="nav-link">
+                  Content List
+                </Link>
+              </nav>
+            </nav>
+          </nav>
+        </header>
+
+        <div className="wrapper">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={(props) => <CreateContent {...props} />}
+            />
+            <Route
+              exact
+              path="/create-content"
+              component={(props) => <CreateContent {...props} />}
+            />
+            <Route
+              exact
+              path="/edit-content/:id"
+              component={(props) => <EditContent {...props} />}
+            />
+            <Route
+              exact
+              path="/content-list"
+              component={(props) => <ContentList {...props} />}
+            />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }

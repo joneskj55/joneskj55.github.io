2b2036cc514d17bb19077f49e90835672a7ca5f4
joneskj55 <joneskj55@gmail.com>
2022-11-09 14:01:01 -0600

    add tailwind forms

--- joneskj55.github.io/src/components/CreateVisitor.js
+++ joneskj55.github.io/src/components/CreateVisitor.js
@@ -46,27 +46,53 @@ export default class CreateVisitor extends Component {
 
   render() {
     return (
-      <div>
+      <div className="grid h-screen place-items-center">
         <form onSubmit={this.onSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              value={this.state.name}
-              onChange={this.createVisitorName}
-              required
-            />
-          </label>
-          <label>
-            Email
-            <input
-              type="email"
-              value={this.state.email}
-              onChange={this.createVisitorEmail}
-              required
-            />
-          </label>
-          <button>Add Name</button>
+          <div className="overflow-hidden shadow sm:rounded-md">
+            <div className="bg-white px-4 py-5 sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.createVisitorName}
+                    required
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.createVisitorEmail}
+                    required
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Add Name
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );

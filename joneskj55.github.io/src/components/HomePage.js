845fb600737493448d373782976edd30dfffd4ab
joneskj55 <joneskj55@gmail.com>
2022-11-10 10:22:01 -0600

    initial home page complete

--- joneskj55.github.io/src/components/HomePage.js
+++ joneskj55.github.io/src/components/HomePage.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FaGithub, FaDev, FaLinkedin } from "react-icons/fa";
+import { SiMongodb, SiExpress, SiReact, SiNodedotjs } from "react-icons/si";
 
 export default class HomePage extends Component {
   render() {
@@ -6,9 +8,38 @@ export default class HomePage extends Component {
       <main className="flex-grow mb-20 m-2 md:m-16">
         <h1 className="text-5xl md:text-7xl font-bold text-center">Hello 👋🏾</h1>
         <br />
-        <p className="text-2xl md:text-3xl font-light mb-5 md:mb-10 text-center">
-          Welcome to my site!
+        <p className="flex flex-1 justify-center text-2xl md:text-3xl font-light mb-5 md:mb-10 text-center">
+          Welcome to my <SiMongodb className="ml-4" /> <SiExpress />
+          <SiReact /> <SiNodedotjs className="mr-4" /> stack site!
         </p>
+        <p className="p-14 ">
+          I'm Kevin Jones, Veteran & Software Engineer. This intentionally
+          over-engineered site is designed to display my full-stack
+          capabilities. This site was built using the MERN stack with Tailwind
+          for styling. If you check out the links in the nav bar you will find
+          my resume/ work history. The projects link will provide some examples
+          of my work, all of which are live websites. If you click on the
+          visitors link you can add your info to the table of visitors of this
+          site. The front end of this app is built with React and Tailwind.
+          Express and Node.js are used on the backend and MongoDB is the
+          database used for storage. As you probably know, most of this stuff,
+          specifically the links to projects and resume could have all been done
+          through the client side. However, the goal of this site is to display
+          full-stack capabilities. So please, poke around the site, check out my
+          socials, and feel free to reach out!{" "}
+        </p>
+        <div className="flex flex-1 justify-center">
+          <a href={"https://linkedin.com/in/joneskj55"} target={"_blank"}>
+            {" "}
+            <FaLinkedin className="text-5xl mr-8 hover:text-gray-400" />
+          </a>
+          <a href={"https://github.com/joneskj55"} target={"_blank"}>
+            <FaGithub className="text-5xl mr-8 hover:text-gray-400" />
+          </a>
+          <a href={"https://dev.to/joneskj55"} target={"_blank"}>
+            <FaDev className="text-5xl hover:text-gray-400" />
+          </a>
+        </div>
       </main>
     );
   }

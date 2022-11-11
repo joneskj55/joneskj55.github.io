4a4b934199c92f9eec86e7be76e9b1ea45ca8a25
joneskj55 <joneskj55@gmail.com>
2022-11-10 18:02:28 -0600

    add box arrow up right to each project tile

--- joneskj55.github.io/src/components/ProjectTile.js
+++ joneskj55.github.io/src/components/ProjectTile.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 export default class ProjectTile extends Component {
   render() {
     return (
       <ul className="hover:shadow-xl">
-        <a href={this.props.obj.url} className="hover:shadow-xl">
+        <a
+          href={this.props.obj.url}
+          target={"_blank"}
+          rel="noreferrer"
+          className="hover:shadow-xl"
+        >
           <div className="rounded overflow-hidden shadow-lg p-2 w-full h-full">
             <div className="px-6 py-4 flex-1">
               <div className="font-bold text-xl mb-2">
+                <span className="flex flex-1 justify-end">
+                  <BsBoxArrowUpRight />
+                </span>
                 {this.props.obj.title}
               </div>
               <p className="text-gray-600 text-base">

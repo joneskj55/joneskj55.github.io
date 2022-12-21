cf0089a04f567e60518120ed95ad4febc4739475
joneskj55 <joneskj55@gmail.com>
2022-12-21 12:33:22 -0600

    remove bg hover state for intro keywords

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -29,41 +29,27 @@ export default class HomePage extends Component {
         <p className="lg:text-lg pt-8 pb-8 m-2 sm:p-14 font-light">
           I'm Kevin Jones, Technologist & Software Engineer. This intentionally
           over-engineered site is designed to display my{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            full-stack
-          </span>{" "}
+          <span className="half-bg cursor-default">full-stack</span>{" "}
           capabilities. This site was built using the MERN stack with{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            Tailwind
-          </span>{" "}
-          for styling. If you{" "}
+          <span className="half-bg cursor-default">Tailwind</span> for styling.
+          If you{" "}
           <span className="hidden lg:contents ">
             check out the links in the nav bar or simply{" "}
           </span>{" "}
           scroll down the page you will find my resume/ work history. The
           projects section will provide some examples of my work, all of which
           are live websites. The front end of this app is built with{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            React
-          </span>{" "}
-          and Tailwind.{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            Express
-          </span>{" "}
-          and{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            Node.js
-          </span>{" "}
-          are used on the backend and{" "}
-          <span className="half-bg hover:half-bg-hover cursor-default">
-            MongoDB
-          </span>{" "}
-          is the database used for storage. As you probably know, most of this
-          stuff, specifically the project data, could have all been handled on
-          the client side. However, the goal of this site is to display
-          full-stack capabilities. So please, poke around the site, check out my
-          socials, add your name to the table in the visitors section, and feel
-          free to reach out!{" "}
+          <span className="half-bg cursor-default">React</span> and Tailwind.{" "}
+          <span className="half-bg cursor-default">Express</span> and{" "}
+          <span className="half-bg cursor-default">Node.js</span> are used on
+          the backend and{" "}
+          <span className="half-bg cursor-default">MongoDB</span> is the
+          database used for storage. As you probably know, most of this stuff,
+          specifically the project data, could have all been handled on the
+          client side. However, the goal of this site is to display full-stack
+          capabilities. So please, poke around the site, check out my socials,
+          add your name to the table in the visitors section, and feel free to
+          reach out!{" "}
         </p>
         <Resume />
         <OpenSource />
